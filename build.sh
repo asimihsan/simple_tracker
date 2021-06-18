c4719cb66f0a7261e96042a8c33d3845c3df02de
Asim Ihsan <asim.ihsan@gmail.com>
2021-06-17 18:02:23 -0700

    fix lambda build

--- build.sh
+++ build.sh
@@ -14,9 +14,8 @@ mkdir -p lambda/proto &&
 protoc -I=proto --dart_out=flutter/lib/proto --go_out=. proto/user.proto proto/calendar.proto
 protoc -I=proto --dart_out=flutter/lib/proto proto/app_preferences.proto
 
-(cd lambda && go build -o build/main *.go)
-
-#(cd lambda && docker run -v $(pwd):/mnt/source -t simple-tracker-lambda /bin/bash -c 'cd /mnt/source && /usr/local/go/bin/go build -o build/main *.go')
-#(cd ephemeral-key-lambda && GOOS=linux go build -o build/main *.go)
+(cd lambda && GOOS=linux go build -o build/main *.go)
+(cd ephemeral-key-lambda && GOOS=linux go build -o build/main *.go)
+(cd cdk && npm run build && npm run test)
 
 # (cd cdk && npm run build && npm run test && cdk deploy preprod-SimpleTrackerCdkStack --strict)
--- lambda/go.mod
+++ lambda/go.mod
@@ -7,10 +7,11 @@ require (
 	github.com/aws/aws-sdk-go v1.33.1
 	github.com/aws/aws-xray-sdk-go v1.1.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/protobuf v1.4.2
+	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.1.1
 	github.com/oklog/ulid v1.3.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/valyala/gozstd v1.7.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	google.golang.org/protobuf v1.26.0
 )
