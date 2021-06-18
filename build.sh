5bc03fe5412285d1203df07b8c88d5632a334c88
Asim Ihsan <ssh@sshemail.ihsan.io>
2021-06-17 23:06:39 -0700

    build.sh - fix typos

--- build.sh
+++ build.sh
@@ -9,13 +9,15 @@ rm -rf flutter/lib/proto
 mkdir -p flutter/lib/proto
 
 rm -rf lambda/proto
-mkdir -p lambda/proto &&
+mkdir -p lambda/proto
 
 protoc -I=proto --dart_out=flutter/lib/proto --go_out=. proto/user.proto proto/calendar.proto
 protoc -I=proto --dart_out=flutter/lib/proto proto/app_preferences.proto
 
 (cd lambda && GOOS=linux go build -o build/main *.go)
 (cd ephemeral-key-lambda && GOOS=linux go build -o build/main *.go)
+
+(cd cdk && npm install)
 (cd cdk && npm run build && npm run test)
 
 # (cd cdk && npm run build && npm run test && cdk deploy preprod-SimpleTrackerCdkStack --strict)
